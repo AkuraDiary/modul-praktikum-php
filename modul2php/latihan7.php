c3e3a17da2b9749ccb09a1549ff90f32e097a74d
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-09-06 09:55:27 +0700

    tambahan latihan

--- modul2php/latihan7.php
+++ modul2php/latihan7.php
@@ -1,6 +1,8 @@
 <?php
     $arrBuah =  array("Mangga", "Apel", "Pisang", "Jeruk");
+
     echo $arrBuah[0]; //Mangga
+    echo"<br>";
     echo $arrBuah[3]; //Jeruk
 
     $arrWarna = array();
@@ -9,6 +11,8 @@
     $arrWarna[] = "Hijau";
     $arrWarna[] = "Putih";
 
+    echo"<br>";
     echo $arrWarna[0]; //Merah
+    echo"<br>";
     echo $arrBuah[2]; //Hijau
 ?>
