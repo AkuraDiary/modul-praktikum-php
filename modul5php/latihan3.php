0bcd5d41a0e4c6e7e9b54b19a1723e475978a01c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-10-06 08:14:34 +0700

    tambahan latihan 2 & 3

---
+++ modul5php/latihan3.php
@@ -0,0 +1,11 @@
+<?php
+    //fungsi tanpa paremeter dan return value
+    function head($jari){
+        echo'
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <link rel="stylesheet" type="text/css" href="style/font-awesome.css">
+        ';
+    }
+    //pemanggilan fungsi
+    
+?>
