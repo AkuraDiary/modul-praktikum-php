e77d039c63b939a355cdccd4bfd4767d1fa63213
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-18 08:43:47 +0700

    hmmmm iso

--- modul7php/siuniv/login/login.php
+++ modul7php/siuniv/login/login.php
@@ -1,11 +1,11 @@
 <?php
-    include "connect.php";
+    include "../connect.php";
     session_start();
     //login process
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if (!isset($username) or !isset($password)) {
+    if ($username == "" or $password == "") {
         echo "<script>alert('Username atau Password kosong');location.href='form_login.php';</script>";
         //header("location: form_login.php");
     }else {
@@ -20,11 +20,11 @@
             $_SESSION['password'] = $password;
             $_SESSION['level'] = $data['level'];
 
-            if ($_session['level'] == "admin") {
+            if ($_SESSION['level'] == "admin") {
                 header("location: home_admin.php");
-            }elseif($_session['level'] == "petugas") {
+            }elseif($_SESSION['level'] == "petugas") {
                 header("location: home_petugas.php");
-            }elseif($_session['level'] == "user") {
+            }else{
                 header("location: home_user.php");
             }
 
